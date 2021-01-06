839bc6f5456766cda9c984eee50875807e70cb82
Alessandro Cipriani <social@magiaslab.com>
2021-01-07 00:14:46 +0100

    title

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Alessandro Cipriani'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Alessandro Cipriani Blog next'
 
 export default function Layout({ children, home }) {
   return (
